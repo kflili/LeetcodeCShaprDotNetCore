26cbb96c393cfd40825028072537b53f12aa2bef
LiLi <kflili@gmail.com>
2017-08-15 18:45:19 -0400

    Add project files.

---
+++ LeetcodeCShaprDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetcodeCShaprDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
