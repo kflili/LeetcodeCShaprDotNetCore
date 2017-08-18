891a8d9ebb15d14bd199a8634715c69a86db5fd6
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-17 21:49:55 -0400

    add notes for 283.

--- Algorithms Basics/Chapter 1. Array_String/26. Remove Duplicates from Sorted Array/similar problems/283. Move Zeroes.cs
+++ Algorithms Basics/Chapter 1. Array_String/26. Remove Duplicates from Sorted Array/similar problems/283. Move Zeroes.cs
@@ -1,4 +1,10 @@
-﻿class Solution {
+﻿/*
+When fast pointer find non zoro item, the slow pointer
+need keep moving until reach zero item, then swap slow
+and fast item.
+ */
+
+class Solution {
     public void MoveZeroes(int[] nums) {
         if (nums == null || nums.Length == 0) {
             return;
