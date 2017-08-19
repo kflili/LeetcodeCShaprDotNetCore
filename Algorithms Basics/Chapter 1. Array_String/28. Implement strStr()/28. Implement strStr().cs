3a54fd67cb57ce3027b0bd94c426e7dfa3f00f8b
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-18 22:16:03 -0400

    add code for 3 and 28

---
+++ Algorithms Basics/Chapter 1. Array_String/28. Implement strStr()/28. Implement strStr().cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        if (haystack == null || needle == null) {
+            return -1;
+        }
+        int m = haystack.Length, n = needle.Length;
+        if (n == 0) return 0;
+        if (m < n) return -1;
+        for (int i = 0; i < m - n; i++) {
+            int j = 0;
+            for (j = 0; j < n; j++) {
+                if (haystack[i + j] != needle[j])
+                    break;
+            }
+            if (j == n) return i;
+        }
+        return -1;
+    }
+}
