18fafd67f20ac0262fc37e154debf707ea59ec4a
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-21 16:13:00 -0400

    add code of 24

---
+++ Algorithms Basics/Chapter 2. Linked List/24. Swap Nodes in Pairs.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public ListNode SwapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy, temp = null;
+        while (head != null && head.next != null) {
+            ListNode n1 = head, n2 = head.next;
+            //       head
+            // pre -> n1 -> n2 -> temp...
+            // pre -> n2 -> n1 -> temp...
+            temp = n2.next;
+            pre.next = n2;
+            n2.next = n1;
+            n1.next = temp;
+            //                          n2 -> pre-> head
+            // move to next pair, ...-> n2 -> n1 -> temp ...
+            pre = pre.next.next;
+            head = temp;
+        }
+        return dummy.next;
+    }
+}
