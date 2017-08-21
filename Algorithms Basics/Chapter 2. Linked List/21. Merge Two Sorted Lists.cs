a8256f5e10d3fb7e8d061f5cd016d73c42ad17b7
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-21 11:18:46 -0400

    add code of 21

---
+++ Algorithms Basics/Chapter 2. Linked List/21. Merge Two Sorted Lists.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if (l1 != null) {
+            tail.next = l1;
+        } else {
+            tail.next = l2;
+        }
+        return dummy.next;
+    }
+}
