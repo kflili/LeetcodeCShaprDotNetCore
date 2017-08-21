f81b950402bedeee79bc051dfee640500fa9a035
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-21 10:13:19 -0400

    add code of 141

---
+++ Algorithms Basics/Chapter 2. Linked List/141. Linked List Cycle.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != slow) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
