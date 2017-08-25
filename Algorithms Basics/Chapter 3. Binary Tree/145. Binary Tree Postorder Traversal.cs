28803f9bf97b0331d0c4f67f974519a58971da65
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-25 00:27:07 -0400

    modified 94 and 145

--- Algorithms Basics/Chapter 3. Binary Tree/145. Binary Tree Postorder Traversal.cs
+++ Algorithms Basics/Chapter 3. Binary Tree/145. Binary Tree Postorder Traversal.cs
@@ -14,7 +14,7 @@ public class Solution {
     }
 }
 
-// iteration
+// iteration1: reverse order insert, similar to two stack.
 public class Solution {
     public IList<int> PostorderTraversal(TreeNode root) {
         var res = new List<int>();
@@ -26,6 +26,66 @@ public class Solution {
             res.Insert(0, root.val);
             if (root.left != null) stack.Push(root.left);
             if (root.right != null) stack.Push(root.right);
+        }
+        return res;
+    }
+}
+
+// two stack
+public class Solution {
+    public IList<int> PostorderTraversal(TreeNode root) {
+        var res = new List<int>();
+        if (root == null) return res;
+        // Create two stacks
+        var s1 = new Stack<TreeNode>();
+        var s2 = new Stack<TreeNode>();
+        // push root to first stack
+        s1.Push(root);
+        // Run while first stack is not empty
+        while (s1.Count > 0) {
+            // Pop an item from s1 and push it to s2
+            var temp = s1.Pop();
+            s2.Push(temp);
+            // Push left and right children of removed
+            // item to s1
+            if (temp.left != null) s1.Push(temp.left);
+            if (temp.right != null) s1.Push(temp.right);
+        }
+        // record all itmes in second stack
+        while (s2.Count > 0) {
+            var node = s2.Pop();
+            res.Add(node.val);
+        }
+        return res;
+    }
+}
+// straightforward just like virtually traverse in postorder
+public class Solution {
+    public IList<int> PostorderTraversal(TreeNode root) {
+        var res = new List<int>();
+        if (root == null) return res;
+        var stack = new Stack<TreeNode>();
+        TreeNode lastVisit = null;
+        while (root != null || stack.Count > 0) {
+            while (root != null) {
+                stack.Push(root);
+                root = root.left;
+            }
+            root = stack.Peek();
+            // if root.right == null, then root is leaf, just Add root
+            // if root.right == lastVisit, 
+            // then both left and right are visited, just Add root
+            if (root.right == null || root.right == lastVisit) {
+                res.Add(root.val);
+                // pop root, mark it as lastvisit, and let root = null,
+                // so we can keep going to peek stack top for next step
+                stack.Pop();
+                lastVisit = root;
+                root = null;
+            } else {
+                // root.right not null, and it's not visited
+                root = root.right;
+            }
         }
         return res;
     }
--- Algorithms Basics/Chapter 3. Binary Tree/94. Binary Tree Inorder Traversal.cs
+++ Algorithms Basics/Chapter 3. Binary Tree/94. Binary Tree Inorder Traversal.cs
@@ -1,4 +1,10 @@
 // recursion
+/**
+* 利用栈实现循环先序遍历二叉树
+* 这种实现类似于图的深度优先遍历（DFS）
+* 维护一个栈，将根节点入栈，然后只要栈不为空，出栈并访问，接着依次将访问节点的右节点、左节点入栈。
+* 这种方式应该是对先序遍历的一种特殊实现（看上去简单明了），但是不具备很好的扩展性，在中序和后序方式中不适用
+*/ 
 public class Solution {
     public IList<int> InorderTraversal(TreeNode root) {
         var res = new List<int>();
@@ -14,21 +20,48 @@ public class Solution {
     }
 }
 
-// iteration
+/* 
+* 利用栈模拟递归过程实现循环先序遍历二叉树
+* 这种方式具备扩展性，它模拟递归的过程，将左子树点不断的压入栈，直到null，然后处理栈顶节点的右子树
+*/
+public class Solution {
+    public IList<int> PreorderTraversal(TreeNode root) {
+        var res = new List<int>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (root != null || stack.Count > 0) {
+            while (root != null) {
+                res.Add(root.val);
+                stack.Push(root);
+                root = root.left;
+            }
+            root = stack.Pop();
+            root = root.right;
+        }
+        return res;
+    }
+}
+
+/**
+* 
+* @param root 树根节点
+* 利用栈模拟递归过程实现循环中序遍历二叉树
+* 思想和上面的preOrderStack_2相同，只是访问的时间是在左子树都处理完直到null的时候出栈并访问。
+*/
 public class Solution {
     public IList<int> InorderTraversal(TreeNode root) {
-        var list = new List<int>();
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode cur = root;
+        var res = new List<int>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<TreeNode>();       
         while (cur != null || stack.Count > 0) {
             while (cur != null) {
-                stack.Push(cur);
-                cur = cur.left;
+                stack.Push(root);
+                root = root.left;
             }
-            cur = stack.Pop();
-            list.Add(cur.val);
-            cur = cur.right;
+            root = stack.Pop();
+            res.Add(root.val);
+            root = root.right;
         }
-        return list;
+        return res;
     }
 }
