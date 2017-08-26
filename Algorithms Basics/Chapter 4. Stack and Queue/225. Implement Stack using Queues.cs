013151f3acb9552fb67a6127c81bb0f4385c880d
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-26 00:07:04 -0400

    modified code of 225

--- Algorithms Basics/Chapter 4. Stack and Queue/225. Implement Stack using Queues.cs
+++ Algorithms Basics/Chapter 4. Stack and Queue/225. Implement Stack using Queues.cs
@@ -1,33 +1,24 @@
 public class MyStack {
-    /** q1 only keeps last items, q2 keeps others. */
     private Queue<int> q1;
-    private Queue<int> q2;
 
     /** Initialize your data structure here. */
     public MyStack() {
         q1 = new Queue<int>();
-        q2 = new Queue<int>();
     }
 
     /** Push element x onto stack. */
     public void Push(int x) {
-        // Empty q1 for push
-        while (q1.Count > 0) {
-            q2.Enqueue(q1.Dequeue());
-        }
         q1.Enqueue(x);
+        int size = q1.Count;
+        while (size-- > 1) {
+            int temp = q1.Dequeue();
+            q1.Enqueue(temp);
+        }
     }
 
     /** Removes the element on top of the stack and returns that element. */
     public int Pop() {
-        int top = q1.Dequeue();  // hold this top one until finish prepare new q1;
-        // swap q1 and q2. move all items (but remain one for next time pop) from new q1 to q2
-        var temp = q1;
-        q1 = q2;
-        q2 = temp;
-        while (q1.Count > 1) {
-            q2.Enqueue(q1.Dequeue());
-        }
+        int top = q1.Dequeue();
         return top;
     }
 
@@ -38,7 +29,7 @@ public class MyStack {
 
     /** Returns whether the stack is empty. */
     public bool Empty() {
-        return q1.Count == 0 && q2.Count == 0;
+        return q1.Count == 0;
     }
 }
 
