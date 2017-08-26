0378e053b22d076652c13e78060802fc082ffd48
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-26 18:09:12 -0400

    add general solution for power of 2

--- Algorithms Basics/Chapter 5. Bit Manipulation/231. Power of Two.cs
+++ Algorithms Basics/Chapter 5. Bit Manipulation/231. Power of Two.cs
@@ -1,6 +1,20 @@
+// bit manipulation trick n & (n-1)
 public class Solution {
     public bool IsPowerOfTwo(int n) {
         if (n <= 0) return false;  // 2^n is always positive. 
         return (n & (n - 1) )== 0;  // bit manipulation trick
+    }
+}
+
+// general power problem solution
+public class Solution {
+    public bool IsPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        while (n > 1) {
+            if (n % 2 == 1) return false;
+            n /= 2;
+        }
+        return true;
     }
 }
