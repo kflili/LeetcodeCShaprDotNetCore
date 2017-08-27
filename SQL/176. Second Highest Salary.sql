7b1abdf1233e535f81549c93608690b0dcfed79e
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-27 16:27:11 -0400

    change sql 176

--- SQL/176. Second Highest Salary.sql
+++ SQL/176. Second Highest Salary.sql
@@ -1,9 +1,24 @@
-WITH CTE AS
-(
-    SELECT Id,Salary,
-           ROW_NUMBER() OVER (ORDER BY Salary DESC) AS RwoNumber 
+--solution 1--
+SELECT (
+    SELECT DISTINCT Salary
     FROM Employee
+    ORDER By Salary DESC
+    OFFSET 1 ROW FETCH NEXT 1 ROW ONLY
 )
-SELECT Salary as SecondHighestSalary
-FROM CTE
-WHERE RowNumber = 2
+AS SecondHighestSalary
+
+--solutioin 2, not consider null--
+SELECT TOP 1 Salary
+FROM (
+      SELECT DISTINCT TOP 2 Salary
+      FROM Employee
+      ORDER BY Salary DESC
+      ) AS Emp
+ORDER BY Salary
+
+--solution 3, using Row_Number--
+SELECT Salary FROM (
+    SELECT Salary, ROW_NUMBER() OVER (ORDER BY salary DESC) rn 
+    FROM Employee
+) AS Emp
+WHERE rn = 2; 
