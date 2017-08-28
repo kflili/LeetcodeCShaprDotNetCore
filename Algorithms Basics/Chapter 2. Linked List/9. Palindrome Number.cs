442114b30670bcedc831f351821a93b19f65e79d
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-28 11:05:46 -0400

    add one more sulution nto 9

--- Algorithms Basics/Chapter 2. Linked List/9. Palindrome Number.cs
+++ Algorithms Basics/Chapter 2. Linked List/9. Palindrome Number.cs
@@ -1,3 +1,29 @@
+// check left == right.
+// left = x / div; right = x % 10;
+// how to find div? note: x / div < 10
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        if (x < 0) return false;
+        int div = 1;
+        // find the intial value of div
+        while (x / div >= 10) {
+            div *= 10;
+        }
+        while (x > 0) {
+            int l = x / div;
+            int r = x % 10;
+            if (l != r) return false;
+            // remove both the left and right number
+            x = (x % div) / 10;
+            // removed two numbers, so div = div / 100;
+            div /= 100;
+        }
+        return true;
+    }
+}
+
+// reverse the second half
 public class Solution {
     public bool IsPalindrome(int x) {
         if (x < 0 || (x != 0 && x % 10 == 0)) return false;
