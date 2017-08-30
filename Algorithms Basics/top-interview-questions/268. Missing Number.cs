45d0afff86fe358af06ed5221936c4bccc4bf615
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-29 21:03:06 -0400

    refactor code

--- Algorithms Basics/top-interview-questions/268. Missing Number.cs
+++ Algorithms Basics/top-interview-questions/268. Missing Number.cs
@@ -1,11 +1,10 @@
 // XOR
 public class Solution {
     public int MissingNumber(int[] nums) {
-        int res = 0;
+        int res = nums.Length;
         for (int i = 0; i < nums.Length; i++) {
             res ^= nums[i] ^ i;
         }
-        res ^= nums.Length;
         return res;
     }
 }
