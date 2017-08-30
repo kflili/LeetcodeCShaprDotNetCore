29d83276d9e37bfc9f275a370ec145333571e65d
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-30 10:52:28 -0400

    add solution without %

--- Algorithms Basics/top-interview-questions/412. Fizz Buzz.cs
+++ Algorithms Basics/top-interview-questions/412. Fizz Buzz.cs
@@ -15,3 +15,26 @@ public class Solution {
             return res;
     }
 }
+
+// without using %
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        List<string> res = new List<string>();
+        for (int i = 1, fizz = 1, buzz = 1; i <= n; i++, fizz++, buzz++) {
+            if (fizz == 3 && buzz == 5) {
+                res.Add("FizzBuzz");
+                fizz = 0;
+                buzz = 0;
+            } else if (fizz == 3) {
+                res.Add("Fizz");
+                fizz = 0;
+            } else if (buzz == 5) {
+                res.Add("Buzz");
+                buzz = 0;
+            } else {
+                res.Add(i.ToString());
+            }
+        }
+            return res;
+    }
+}
