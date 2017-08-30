135d6f05482af8f86c5f4e2ba68c1af7ae261a6f
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-08-30 00:46:14 -0400

    add follow up Q&A to 350

--- Algorithms Basics/top-interview-questions/350. Intersection of Two Arrays II.cs
+++ Algorithms Basics/top-interview-questions/350. Intersection of Two Arrays II.cs
@@ -1,3 +1,23 @@
+/*
+Q: What if the given array is already sorted? How would you 
+optimize your algorithm?
+A: If both arrays are sorted, I would use two pointers to 
+iterate, which somehow resembles the merge process in merge sort.
+
+Q: What if nums1's size is small compared to nums2's size? 
+Which algorithm is better?
+A: Suppose lengths of two arrays are N and M, the time complexity 
+of my solution is O(N+M) and the space complexity if O(N) 
+considering the hash. So it's better to use the smaller array 
+to construct the counter hash. Well, as we are calculating the 
+intersection of two arrays, the order of array doesn't matter. 
+We are totally free to swap to arrays.
+
+Q: What if elements of nums2 are stored on disk, and the memory 
+is limited such that you cannot load all elements into the memory 
+at once?
+A: Divide and conquer. Repeat the process frequently: Slice nums2 to fit into memory, process (calculate intersections), and write partial results to memory. */
+
 // Dictionary
 public class Solution {
     public int[] Intersect(int[] nums1, int[] nums2) {
