644c6b312d26126e4a73671f354ca9e9e9f9dd89
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-09-02 16:58:06 -0400

    add code of 19

---
+++ top-interview-questions/19. Remove Nth Node From End of List.cs
@@ -0,0 +1,17 @@
+// two pointer, mantain the gap = n between two pointers.
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy, slow = dummy;
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
