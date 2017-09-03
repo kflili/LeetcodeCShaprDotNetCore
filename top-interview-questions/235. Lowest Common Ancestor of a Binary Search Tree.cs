cd9aa95fbd02d08e3faeb4453680b73fbd42b250
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-09-03 15:49:24 -0400

    add code of 235

--- top-interview-questions/235. Lowest Common Ancestor of a Binary Search Tree.cs
+++ top-interview-questions/235. Lowest Common Ancestor of a Binary Search Tree.cs
@@ -1,3 +1,4 @@
+// general one for BT not only BST
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) return null;
@@ -7,5 +8,18 @@ public class Solution {
         if (left != null && right != null) return root;
         else if (left == null) return right;
         else return left;
+    }
+}
+
+// specific for BST
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return LowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return LowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
     }
 }
