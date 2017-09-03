541c5ed92f10b9aaf6ce24b5cd0b5a9165d93540
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-09-03 17:02:04 -0400

    add one more vary solution to 236

--- top-interview-questions/236. Lowest Common Ancestor of a Binary Tree.cs
+++ top-interview-questions/236. Lowest Common Ancestor of a Binary Tree.cs
@@ -8,3 +8,35 @@ public class Solution {
         return left == null ? right : left;
     }
 }
+
+// if there are parent for each node, the solution can be changed
+public ParentTreeNode lowestCommonAncestorII(ParentTreeNode root,
+                                                ParentTreeNode A,
+                                                ParentTreeNode B) {
+    ArrayList<ParentTreeNode> pathA = getPath2Root(A);
+    ArrayList<ParentTreeNode> pathB = getPath2Root(B);
+    
+    int indexA = pathA.size() - 1;
+    int indexB = pathB.size() - 1;
+    
+    ParentTreeNode lowestAncestor = null;
+    while (indexA >= 0 && indexB >= 0) {
+        if (pathA.get(indexA) != pathB.get(indexB)) {
+            break;
+        }
+        lowestAncestor = pathA.get(indexA);
+        indexA--;
+        indexB--;
+    }
+    
+    return lowestAncestor;
+}
+
+private ArrayList<ParentTreeNode> getPath2Root(ParentTreeNode node) {
+    ArrayList<ParentTreeNode> path = new ArrayList<>();
+    while (node != null) {
+        path.add(node);
+        node = node.parent;
+    }
+    return path;
+}
