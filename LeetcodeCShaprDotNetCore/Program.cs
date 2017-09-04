c03ba2bc02240389476868d6dc18d63d6fc37440
Li Li <lili@lis-macbook-pro.attlocal.net>
2017-09-03 23:42:55 -0400

    add code 151

--- LeetcodeCShaprDotNetCore/Program.cs
+++ LeetcodeCShaprDotNetCore/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 namespace LeetcodeCShaprDotNetCore {
     class Program {
         static void Main(string[] args) {
-            
+            Solution s = new Solution();
+            string str = "hello world!";
+            string newS = s.ReverseWords(str);
+            System.Console.WriteLine(newS);
         }
     }
 }
